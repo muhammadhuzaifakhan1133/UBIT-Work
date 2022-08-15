4f1b5fd7c4b4292d7a3778d8296e3f7b5e2d1c3f
Muhammad Huzaifa Khan <huzaifaacademy11@gmail.com>
2022-08-15 22:38:40 +0500

    update get_htmlPage.py by removing decode function

--- Semaster 2/OOP/urllib/get_htmlpage.py
+++ Semaster 2/OOP/urllib/get_htmlpage.py
@@ -3,7 +3,5 @@ from urllib import request
 URL = "https://oxpstopdf.com"
 webConn = request.urlopen(URL)
 webBytes = webConn.read()
-webString = webBytes.decode("cp1252")
-file = open("oxpstopdf", "w")
-file.write(webString)
-file.close()
+with open("oxpstopdf.html", "wb+") as file:
+    file.write(webBytes)
